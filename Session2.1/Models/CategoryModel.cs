4e725e15cdbd53d61377faab9159db1b3af302b7
Abigail Jumeirah Castro <abigailjumeirah.c@ph.softvision.global>
2023-03-09 19:08:03 +0800

    Completed session 2.1 homework

---
+++ Session2.1/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace APITraining.Session2._1
+{
+    public class CategoryModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
